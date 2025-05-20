ea40080a243cee6b7eec1d792874e98a0b34f2ac
raghadf2 <148901683+raghadf2@users.noreply.github.com>
2025-05-20 23:56:48 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const cacheName = "calc-v1";
+const filesToCache = [
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
